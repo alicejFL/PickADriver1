eece420dbcef1887c8d1bf76a553fba5c81db9c5
Alice <alicej@psdschools.org>
2021-02-17 09:30:33 -0700

    configure name rearranging

--- PickADriver1/NamesTableViewController.swift
+++ PickADriver1/NamesTableViewController.swift
@@ -11,14 +11,27 @@ class NamesTableViewController: UITableViewController {
 var names = ["Kelly", "Scott", "Heather", "Hilary", "Kris", "Chef", "DJ", "Ryan", "John", "Noah", "Tim", "Brooks"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.isEditing = true
     }
 
+    @IBAction func onAddNameTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add a name", message: nil, preferredStyle: .alert)
+        var nameField: UITextField?
+        alert.addTextField(configurationHandler: {(textField) in
+            nameField = textField
+            nameField?.placeholder = "Name"
+        })
+        let cancelAction = UIAlertAction(title: "Cance;", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let confirmAction = UIAlertAction(title: "OK", style: .default, handler: {(action) in
+            if let name = nameField?.text {
+                self.names.append(name)
+                self.tableView.reloadData()
+            }
+        })
+        alert.addAction(confirmAction)
+        present(alert, animated: true, completion: nil)
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -39,24 +52,41 @@ var names = ["Kelly", "Scott", "Heather", "Hilary", "Kris", "Chef", "DJ", "Ryan"
     }
     
 
-    @IBAction func onAddNameTapped(_ sender: Any)
-    {
-        let alert = UIAlertController(title: "Add a name", message: nil, preferredStyle: .alert)
-        var nameField: UITextField?
-        alert.addTextField(configurationHandler: {(textField) in
-            nameField = textField
-            nameField?.placeholder = "Name"
-        })
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alert.addAction(cancelAction)
-        let confirmAction = UIAlertAction(title: "OK", style: .default, handler: {(action) in
-            if let name = nameField?.text {
-                self.names.append(name)
-                self.tableView.reloadData()
-            }
-        })
-        alert.addAction(confirmAction)
-        present(alert, animated: true, completion: nil)
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
     }
     
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+         //    Delete the row from the data source
+            names.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+
+    
+     //Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let nameToMove = names.remove(at: fromIndexPath.row)
+        names.insert(nameToMove, at: to.row)
+    }
+    
+
+    
+     ///Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     //    Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    
+
+
+
 }
